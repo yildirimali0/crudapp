8d088790bcf8ed537212e7f0a1fe1a18af57cc33
yildirimali0 <yildirimaliyasir97@gmail.com>
2023-05-31 19:06:29 +0300

    Updated according to BEM

--- crudapp.js
+++ crudapp.js
@@ -1,126 +1,127 @@
-var selectedRow=null
+var selectedRow = null
 
-//alert box
-function showAlerts(message,className){
+// Alert box
+function showAlerts(message, className) {
     const div = document.createElement("div")
     div.className = `alert alert-${className}`
 
-    div.appendChild(document.createTextNode(message))
-    const container = document.querySelector(".container1")
-    const main = document.querySelector("#form")
-    container.insertBefore(div,main)
-    const formWidth = document.querySelector("#form").offsetWidth
-    div.style.width = `${formWidth}px`
-    div.style.marginLeft = `auto`
-    div.style.marginRight = `auto`
-    //time for the box
-    setTimeout(() => document.querySelector(".alert").remove(),3000)
+    div.appendChild(document.createTextNode(message)) 
+    const container = document.querySelector(".container") 
+    const main = document.querySelector(".form") 
+    container.insertBefore(div, main) 
+    const formWidth = document.querySelector(".form").offsetWidth 
+    div.style.width = `${formWidth}px` 
+    div.style.marginLeft = `auto` 
+    div.style.marginRight = `auto` 
+    div.style.textAlign = "left";
+
+    // Time for the box
+    setTimeout(() => document.querySelector(".alert").remove(), 3000) 
 }
 
-//form clearing
+// Form clearing
 function clearFields() {
-    document.querySelector("#name").value = ""
-    document.querySelector("#surname").value = ""
-    document.querySelector("#city").value = ""
-    document.querySelector("#tel").value = ""
+    document.querySelector(".form--name").value = "" 
+    document.querySelector(".form--surname").value = "" 
+    document.querySelector(".form--city").value = "" 
+    document.querySelector(".form--tel").value = "" 
 }
 
 // Load data from localStorage
 document.addEventListener("DOMContentLoaded", () => {
-    let dataArray = JSON.parse(localStorage.getItem("data")) || []
-    populateTable(dataArray)
-})
+    let dataArray = JSON.parse(localStorage.getItem("data")) || [] 
+    populateTable(dataArray) 
+}) 
 
-// delete data from localstrage
+// Delete data from localStorage
 function removeDataFromLocalStorage(row) {
-    let dataArray = JSON.parse(localStorage.getItem("data")) || []
-    const index = row.rowIndex - 1
-    dataArray.splice(index, 1)
-    localStorage.setItem("data", JSON.stringify(dataArray))
+    let dataArray = JSON.parse(localStorage.getItem("data")) || [] 
+    const index = row.rowIndex - 1 
+    dataArray.splice(index, 1) 
+    localStorage.setItem("data", JSON.stringify(dataArray)) 
 }
 
-//delete data
-document.querySelector("#list").addEventListener("click", (e) => {
-    var target = e.target
+// Delete data
+document.querySelector(".list").addEventListener("click", (e) => {
+    var target = e.target 
     if (target.classList.contains("delete")) {
-        const row = target.parentElement.parentElement
-        row.remove()
-        showAlerts("Bilgiler Silindi...", "danger")
-        removeDataFromLocalStorage(row)
+        const row = target.parentElement.parentElement 
+        row.remove() 
+        showAlerts("Bilgiler Silindi...", "danger") 
+        removeDataFromLocalStorage(row) 
     }
-})
+}) 
 
-
-//add data
-document.querySelector("#form").addEventListener("submit",(e) =>{
-    e.preventDefault()
-    // get form values
-    const name = document.querySelector("#name").value
-    const surname = document.querySelector("#surname").value
-    const city = document.querySelector("#city").value
-    const tel = document.querySelector("#tel").value
-    //validate
-    if(name == "" || surname=="" || city=="" || tel==""){
-        showAlerts("Lütfen Alanları Boşluksuz Doldurun","danger")
+// Add data
+document.querySelector(".form").addEventListener("submit", (e) => {
+    e.preventDefault() 
+    // Get form values
+    const name = document.querySelector(".form--name").value 
+    const surname = document.querySelector(".form--surname").value 
+    const city = document.querySelector(".form--city").value 
+    const tel = document.querySelector(".form--tel").value 
+    // Validate
+    if (name == "" || surname == "" || city == "" || tel == "") {
+        showAlerts("Lütfen Alanları Boşluksuz Doldurun", "danger") 
     } else {
         const data = {
             name: name,
             surname: surname,
             city: city,
             tel: tel,
-        }
+        } 
 
-        let dataArray = JSON.parse(localStorage.getItem("data")) || [];
+        let dataArray = JSON.parse(localStorage.getItem("data")) || [] 
         if (selectedRow == null) {
-            dataArray.push(data)
-            showAlerts("Bilgi Eklendi", "success")
-            clearFields()
+            dataArray.push(data) 
+            showAlerts("Bilgi Eklendi", "success") 
+            clearFields() 
         } else {
-            const selectedIndex = selectedRow.rowIndex - 1
-            dataArray[selectedIndex] = data
-            showAlerts("Bilgiler Güncellendi", "info")
-            clearFields()
+            const selectedIndex = selectedRow.rowIndex - 1 
+            dataArray[selectedIndex] = data 
+            showAlerts("Bilgiler Güncellendi", "success") 
+            clearFields() 
         }
 
-        populateTable(dataArray)
+        populateTable(dataArray) 
 
         // Save data to localStorage
-        localStorage.setItem("data", JSON.stringify(dataArray))
-        selectedRow = null
+        localStorage.setItem("data", JSON.stringify(dataArray)) 
+        selectedRow = null 
     }
-})
+}) 
 
-//Edit row
-document.querySelector("#list").addEventListener("click", (e) => {
-    var target = e.target
+// Edit row
+document.querySelector(".list").addEventListener("click", (e) => {
+    var target = e.target 
     if (target.classList.contains("edit")) {
-        selectedRow = target.parentElement.parentElement
-        document.querySelector("#name").value = selectedRow.children[0].textContent
-        document.querySelector("#surname").value = selectedRow.children[1].textContent
-        document.querySelector("#city").value = selectedRow.children[2].textContent
-        document.querySelector("#tel").value = selectedRow.children[3].textContent
+        selectedRow = target.parentElement.parentElement 
+        document.querySelector(".form--name").value = selectedRow.children[0].textContent 
+        document.querySelector(".form--surname").value = selectedRow.children[1].textContent 
+        document.querySelector(".form--city").value = selectedRow.children[2].textContent 
+        document.querySelector(".form--tel").value = selectedRow.children[3].textContent 
     }
-})
+}) 
 
-//Populates the table
+// Populates the table
 function populateTable(dataArray) {
-    const lis = document.querySelector("#list")
-    lis.innerHTML = ""
+    const list = document.querySelector(".list") 
+    list.innerHTML = "" 
 
     dataArray.forEach((data, index) => {
-        const row = document.createElement("tr")
+        const row = document.createElement("tr") 
 
         row.innerHTML = `
-            <td>${data.name}</td>
-            <td>${data.surname}</td>
-            <td>${data.city}</td>
-            <td>${data.tel}</td>
-            <td>
-                <a href="#" class="btn btn-warning btn-sm mr-2 text-white edit">DÜZENLE</a>
-                <a href="#" class="btn btn-danger btn-sm text-white delete">SİL</a>
-            </td> 
-        `
-        lis.appendChild(row)
-    })
-}
+      <td>${data.name}</td>
+      <td>${data.surname}</td>
+      <td>${data.city}</td>
+      <td>${data.tel}</td>
+      <td>
+        <a href="#" class="edit">DÜZENLE</a>
+        <a href="#" class="delete">SİL</a>
+      </td>
+    ` 
 
+        list.appendChild(row) 
+    }) 
+}
